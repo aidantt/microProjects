41b91eb7285ab74644c006d1b4514fc8cc16fe5d
aidantt <aidant7376@gmail.com>
2023-04-24 15:50:16 -0500

    update rust_collections

--- rust_collections/src/main.rs
+++ rust_collections/src/main.rs
@@ -1,6 +1,7 @@
 // exploring some of rust's collections, unique and helpful
 // data structures that COME FREE WITH YOUR XBOX (std library)
 
+use std::collections::HashMap;
 
 fn main() {
     // vectors store more than one value of the same type in
@@ -65,4 +66,80 @@ fn main() {
         SpreadsheetCell::Text(String::from("blue")),
         SpreadsheetCell::Float(10.12),
     ];
+
+    // And now, for the String type that comes with the standard library.
+    //
+    // In essence, strings are a list of characters, represented in UTF-8
+    // encoding using 4-bit or 8-bit character variables.
+    // In rust, strings are implemented as a collection of bytes, and the
+    // String data structure gives some helpful methods to construct and
+    // modify strings while preserving ownership and code safety on the heap.
+    //
+    // Rust only has one string type in the core language, which is the
+    // "str" string slice. A slice is just a pre-defined list of values.
+    // For example, a string literal that is known at compile is a string slice
+    // of type str.
+    //
+    // By comparison, the String type is provided by the standard library, and
+    // acts more like a data structure than a compile time variable.
+    // String is a growable, mutable, owned, UTF-8 encoded string type.
+    // Similar to Vec<T>, String is instantiated and manipulated using
+    // dot notation and method construction:
+    let mut s = String::new(); // instantiates an empty String
+    
+    // String methods can be used on string literals
+    let data = "initial contents"; // data is a string literal str
+
+    let s = data.to_string(); // to_string() is a method of String
+
+    // this method also works on literals directly:
+    let s = "initial contents".to_string();
+
+    println!("data: {data}");
+    println!("s: {s}");
+
+    // String also has a method from() that can encode string literals
+    let s = String::from("from contents");
+    println!("from string: {s}");
+
+    // The push_str() method can append string literals to mutable Strings:
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    println!("s2 is {s2}");
+    println!("s1 is {s1}");
+
+    // lastly, a less common but vital collection data structure that rust
+    // provides is the Hash Map.
+    //
+    // In rust, hash maps take the form of a data structure like HashMap<K, V>.
+    // This type stores a mapping of keys of type K to values of type V using
+    // a hashing function.
+    //
+    // hash maps are useful when you want to look up data not by index,
+    // but by some known key. Accessing data in a hash map is more complicated
+    // than an array, but can be much faster.
+    //
+    // To start, instantiate an empty hash map:
+    let mut scores = HashMap::new();
+
+    // Then, the insert() method can input key value pairs into the map:
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    // using the get() method and passing the key, we can extract the value
+    // associated with the key:
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+
+    // get(&K) will return Option<&V>. If there's no value for the key, get()
+    // will return None.
+    // In the above code, the Option is handled by using copied() to get an
+    // Option<T> rather than an Option<&T>, then an unwrap_or(0) to unwrap
+    // the data from the Option and set the value to 0 if the Option is None.
+    //
+    // Iterating over hash maps are also possible:
+    for (key, value) in &scores {
+        println!("{key}: {value}");
+    }
 }
