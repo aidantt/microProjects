41484b886b30742dde12ef920b7323861c141db1
aidantt <aidant7376@gmail.com>
2023-04-24 12:22:52 -0500

    update rust_collections

---
+++ rust_collections/target/debug/deps/rust_collections-a7d20147aea95266.d
@@ -0,0 +1,5 @@
+/home/aidant/sillyContraptions/microProjects/rust_collections/target/debug/deps/rust_collections-a7d20147aea95266: src/main.rs
+
+/home/aidant/sillyContraptions/microProjects/rust_collections/target/debug/deps/rust_collections-a7d20147aea95266.d: src/main.rs
+
+src/main.rs:
---
+++ rust_collections/target/debug/rust_collections.d
@@ -0,0 +1,1 @@
+/home/aidant/sillyContraptions/microProjects/rust_collections/target/debug/rust_collections: /home/aidant/sillyContraptions/microProjects/rust_collections/src/main.rs
